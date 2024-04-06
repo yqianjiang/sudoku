3372a7305272b3ca3d2768f48af0248636aa368b
yqianjiang <y.jiang.psy@gmail.com>
2024-04-06 17:58:25 +0800

    fix: wasm dir for build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import wasmPack from 'vite-plugin-wasm-pack'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasmPack(['../wasm-sudoku'])],
+  plugins: [react(), wasmPack(['./wasm-sudoku'])],
   build: {
     rollupOptions: {
       input: {
---
+++ wasm-sudoku/pkg/wasm_sudoku_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function generate_sudoku(a: number): void;
+export function solve_sudoku(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_exn_store(a: number): void;
